6a249b3f68653e1825f30e88a425c0a3781f82e7
yuki0924sugi <yuki0924sugi@icloud.com>
2018-04-27 01:30:26 +0900

    tweet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "posts/index" => "posts#index"
+  get "posts/new"=>"posts#new"
+  post "posts/create"=>"posts#create"
+  get "posts/:id"=>"posts#show"
   get "/" => "home#top"
   get "about"=>"home#about"
 end
