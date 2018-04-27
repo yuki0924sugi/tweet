fb3aee6b2f7990aac55523a0462832d5515d9743
yuki0924sugi <yuki0924sugi@icloud.com>
2018-04-27 13:43:53 +0900

    tweet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,18 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts/index")
   end
+  def edit
+    @post=Post.find_by(id: params[:id])
+  end
+  def update
+    @post=Post.find_by(id: params[:id])
+    @post.content=params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+  def destroy
+    @post=Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
 end
