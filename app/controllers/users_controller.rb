10f393184e63cfd26ece0d4680c9aaee8a8b3671
yuki0924sugi <yuki0924sugi@icloud.com>
2018-04-30 22:05:19 +0900

    tweet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user=User.new
   end
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(name: params[:name], email: params[:email], image_name: "ninjawanko.jpg")
     if@user.save
       flash[:notice]="User's registration was completed"
       redirect_to("/users/#{@user.id}")
@@ -24,6 +24,11 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    if params[:image]
+      @user.image_name="#{@user.id}.jpg"
+      image=params[:image]
+      File.binwrite("public/users_images/#{@user.image_name}",image.read)
+    end
     if @user.save
       flash[:notice] = "I edited user infomation"
       redirect_to("/users/#{@user.id}")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180429090533) do
+ActiveRecord::Schema.define(version: 20180430122717) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180429090533) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
